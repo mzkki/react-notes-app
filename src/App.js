c13ecb3ec62b43a2895474116b3df48b7f5d6b04
mzkki <08harismu@gmail.com>
2022-12-26 18:08:56 +0800

    fix container

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React from "react";
 import NotesApp from "./components/notes/NotesApp";
 import { Route, Routes } from "react-router-dom";
 import { DetailNoteWrapper } from "./pages/DetailNote";
-import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <Container>
-        <header>
-          {/* <h2>Note Detail</h2> */}
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<NotesApp />} />
-            <Route path="/note/:id" element={<DetailNoteWrapper />} />
-          </Routes>
-        </main>
-      </Container>
+      <header>
+        {/* <h2>Note Detail</h2> */}
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<NotesApp />} />
+          <Route path="/note/:id" element={<DetailNoteWrapper />} />
+        </Routes>
+      </main>
     </>
   )
 }
