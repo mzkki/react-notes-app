2adb1529c0e872baeeb8df2b45a8dc2cf565ff0d
mzkki <08harismu@gmail.com>
2022-12-28 19:44:19 +0800

    deleting old file and activate the swal

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import NoteDetail from "../components/notes/NoteDetail";
 import { archiveNote, deleteNote, getNote, showFormattedDate } from "../utils";
 import { useNavigate, useParams } from "react-router-dom";
-// import Swal from "sweetalert2";
+import Swal from "sweetalert2";
 
 function DetailPage() {
   const { id } = useParams()
@@ -10,25 +10,25 @@ function DetailPage() {
   const navigate = useNavigate()
 
   function onDeleteHandler(id) {
-    // Swal.fire({
-    //   title: 'Are you sure?',
-    //   text: "You won't be able to revert this!",
-    //   icon: 'warning',
-    //   showCancelButton: true,
-    //   confirmButtonColor: '#3085d6',
-    //   cancelButtonColor: '#d33',
-    //   confirmButtonText: 'Yes, delete it!'
-    // }).then((result) => {
-    //   if (result.isConfirmed) {
-    //     deleteNote(id)
-    //     navigate('/')
-    //     Swal.fire(
-    //       'Deleted!',
-    //       'Catatan kamu berhasil dihapus!',
-    //       'success'
-    //     )
-    //   }
-    // })
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteNote(id)
+        navigate('/')
+        Swal.fire(
+          'Deleted!',
+          'Catatan kamu berhasil dihapus!',
+          'success'
+        )
+      }
+    })
     deleteNote(id);
     navigate('/')
   }
